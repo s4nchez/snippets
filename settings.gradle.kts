2805805b7d9bf322dfb32f60ef0f14f6417f002d
Ivan Sanchez <ivan@gourame.com>
2024-11-07 09:03:25 +0000

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.5"
+}
+rootProject.name = "snippets"
+
